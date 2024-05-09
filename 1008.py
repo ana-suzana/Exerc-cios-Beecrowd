e2005ce8292e9653167253464f93fc7ed2fb4ac5
Ana Tereza Suzana <166082193+ana-suzana@users.noreply.github.com>
2024-05-09 10:24:56 -0300

    Create 1008.py
    Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
    
    Entrada: O arquivo de entrada contém 2 números inteiros e 1 número com duas casas decimais, representando o número, quantidade de horas trabalhadas e o valor que o funcionário recebe por hora trabalhada, respectivamente.
    
    Saída: Imprima o número e o salário do funcionário, conforme exemplo fornecido, com um espaço em branco antes e depois da igualdade. No caso do salário, também deve haver um espaço em branco após o $.

---
+++ 1008.py
@@ -0,0 +1,7 @@
+#salario
+numero = int(input())
+num_horas = int(input())
+valor_hora = float(input())
+salario = num_horas * valor_hora
+print(f"NUMBER = {numero}")
+print(f"SALARY = U$ {salario}")
